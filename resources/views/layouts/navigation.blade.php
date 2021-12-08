17eeb878812f54803bd8464e82b7df20f28b44a9
Fedde van Gils <feddevangils@gmail.com>
2021-12-08 10:12:50 +0100

    change route again

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,7 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('dashboard/events')" :active="request()->routeIs('events')">
+                    <x-nav-link :href="route('events.index')" :active="request()->routeIs('events')">
                         {{ __('Events') }}
                     </x-nav-link>
                 </div>
